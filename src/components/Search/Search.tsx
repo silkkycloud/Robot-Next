45c40639297894715f29873d7d6ef35450685079
Oscar <silkky@silkky.cloud>
2021-12-22 09:17:51 +1100

    Merge SearchSuggestions into Search component

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -1,8 +1,10 @@
-import React, { Dispatch, Fragment, SetStateAction, useState } from 'react'
+import React, { Dispatch, Fragment, SetStateAction, useEffect, useState } from 'react'
 
 import { Dialog, Transition } from '@headlessui/react'
-import SearchSuggestions from './SearchSuggestions'
 import { HiX } from 'react-icons/hi'
+import classNames from '../../functions/classNames'
+import useKeyPress from '../../hooks/useKeyPress'
+import useFetchApi from '../../hooks/useFetchApi'
 
 export type SearchProps = {
   open: boolean
@@ -11,6 +13,81 @@ export type SearchProps = {
 
 const Search = ({open, setOpen}: SearchProps): JSX.Element => {
   const [searchQuery, setSearchQuery] = useState('')
+  const [selected, setSelected] = useState<string>('')
+  const [hovered, setHovered] = useState<string>('')
+  const [cursor, setCursor] = useState<number>()
+
+  const downPress = useKeyPress('ArrowDown')
+  const upPress = useKeyPress('ArrowUp')
+  const enterPress = useKeyPress('Enter')
+
+  const [suggestions] = useFetchApi('/suggestions?query=' + searchQuery)
+
+  useEffect(() => {
+    if (suggestions.length && downPress) {
+      setCursor(prevState => {
+        if (prevState != undefined)
+          if (prevState < suggestions.length - 1)
+            return prevState + 1
+          else
+            return prevState
+        else
+          setCursor(0)
+      })
+    }
+  }, [downPress])
+  useEffect(() => {
+    if (suggestions.length && upPress) {
+      setCursor(prevState => {
+        if (prevState != undefined)
+          if (prevState > 0)
+            return prevState - 1
+          else
+            return prevState
+        else
+          setCursor(0)
+      })
+    }
+  }, [upPress])
+  useEffect(() => {
+    if (suggestions.length && enterPress) {
+      if (cursor != undefined)
+        setSelected(suggestions[cursor])
+    }
+  }, [enterPress, cursor])
+  useEffect(() => {
+    if (suggestions.length && hovered) {
+      setCursor(suggestions.indexOf(hovered))
+    }
+  }, [hovered])
+
+  const suggestionsList: JSX.Element = (
+    <ul
+      role="list"
+      className="overflow-auto max-h-48 snap-y sm:max-h-60 md:max-h-screen"
+    >
+      {suggestions.map((suggestion: string, i: number) =>
+        <li
+          key={i.toString()}
+          className={classNames(
+            i === cursor ? 'bg-gray-50 dark:bg-neutral-900' : '',
+            'rounded-md cursor-pointer snap-center px-3 py-3 sm:px-2 sm:py-2'
+          )}
+          onClick={() => setSelected(suggestion)}
+          onMouseEnter={() => setHovered(suggestion)}
+          onMouseLeave={() => setHovered('')}
+        >
+          <div className="flex-1 space-y-1">
+            <div className="flex items-center justify-between">
+              <p className="text-sm text-gray-500 dark:text-neutral-400">
+                {suggestion}
+              </p>
+            </div>
+          </div>
+        </li>
+      )}
+    </ul>
+  )
 
   return (
     <Transition.Root show={open} as={Fragment}>
@@ -78,10 +155,13 @@ const Search = ({open, setOpen}: SearchProps): JSX.Element => {
                 </div>
                 <div className="px-2">
                   {/* Search Suggestions */}
-                  <SearchSuggestions
-                    query={searchQuery}
-                    setQuery={setSearchQuery}
-                  />
+                  <div className="flex flex-col h-screen sm:h-auto">
+                    <div className="bg-white dark:bg-neutral-800 shadow rounded-md">
+                      <div>
+                        {suggestionsList}
+                      </div>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
