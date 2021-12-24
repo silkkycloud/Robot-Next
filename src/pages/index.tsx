ca468d68d7ddf90d554c3bb7c56a5f31217c8ec8
Oscar <silkky@silkky.cloud>
2021-12-25 01:18:51 +1100

    Add 404 page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import {
 } from 'react-router-dom'
 import { useSnapshot } from 'valtio'
 
+import { Link } from 'react-router-dom'
 import { DefaultSeo } from 'next-seo'
 import { ThemeProvider } from 'next-themes'
 import Nav from '@/components/Nav/Nav'
@@ -19,6 +20,36 @@ import Settings from '@/components/Settings/Settings'
 import Feed from '@/components/Feed/Feed'
 import Subscriptions from '@/components/Subscriptions/Subscriptions'
 import Channel from '@/components/Channel/Channel'
+
+export const NotFound = () => (
+  <div className="bg-white dark:bg-neutral-900 min-h-full px-4 py-16 sm:px-6 sm:py-24 md:grid md:place-items-center lg:px-8">
+    <div className="max-w-max mx-auto">
+      <main className="sm:flex">
+        <h1 className="text-4xl font-extrabold text-red-600 sm:text-5xl">
+          404
+        </h1>
+        <div className="sm:ml-6">
+          <div className="sm:border-l sm:border-gray-200 sm:pl-6">
+            <h1 className="text-4xl font-extrabold text-black dark:text-white tracking-tight sm:text-5xl">
+              Page not found
+            </h1>
+            <p className="mt-1 text-base text-gray-600 dark:text-neutral-400">
+              Please check the URL in the address bar and try again.
+            </p>
+          </div>
+          <div className="mt-10 sm:border-l sm:border-transparent sm:pl-6">
+            <Link
+              to="/"
+              className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-red-600 hover:bg-red-700"
+            >
+              Go back home
+            </Link>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+)
 
 export const ScrollToTop = () => {
   const { pathname } = useLocation()
@@ -72,6 +103,7 @@ const Index = () => {
               path="/user/:id/*"
               element={<Channel channelPrefix="/user/" />}
             />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </Nav>
       </ThemeProvider>
